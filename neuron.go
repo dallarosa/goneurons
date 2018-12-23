14715d1b8afa97c8a6a4f2e95a9b0e747fbe48b5
Francisco Soares <dalla@dallarosa.me>
2018-12-23 12:40:23 +0900

    First implementation of the neuron
    Fixing the Forward Step should make the code support any number of
    features, specified in featSize. At some point this should be infered
    from the input.

---
+++ neuron.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"math/rand"
+	"time"
+)
+
+const (
+	learnRate = 0.001
+	featSize  = 2
+)
+
+type Neuron struct {
+	W    []float64
+	b    float64
+	X    Input
+	y    []float64
+	yHat []float64
+}
+
+type Input [][featSize]float64
+
+func (n *Neuron) Initialize(input Input, output []float64) {
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	n.W = make([]float64, featSize)
+	n.yHat = make([]float64, len(output))
+	for i, _ := range n.W {
+		n.W[i] = r.Float64()
+	}
+	n.b = r.Float64()
+	n.X = input
+	n.y = output
+}
+
+func (n *Neuron) ForwardStep() {
+	for i, v := range n.X {
+		n.yHat[i] = v[0]*n.W[0] + v[1]*n.W[1] + n.b
+	}
+}
+
+func (n *Neuron) Cost() float64 {
+	var cost float64
+	for i, v := range n.y {
+		cost += math.Pow((v - n.yHat[i]), 2)
+	}
+	return cost / float64(len(n.y))
+}
+
+func (n *Neuron) BackwardStep() {
+	for i, _ := range n.W {
+		delta := 0.0
+		for j, v := range n.y {
+			delta += 2 * (v - n.yHat[j]) * n.X[j][i]
+		}
+		delta = delta / float64(len(n.y))
+		n.W[i] += delta * learnRate
+	}
+	delta := 0.0
+	for j, v := range n.y {
+		delta += 2 * (v - n.yHat[j])
+	}
+	delta = delta / float64(len(n.y))
+	n.b += delta * learnRate
+}
+
+func main() {
+	n := new(Neuron)
+	n.Initialize(Input{{1, 2}, {2, 3}, {5, 5}, {4, 3}, {2, 2}, {10, 10}, {6, 4}, {34, 12}}, []float64{3, 5, 10, 7, 4, 20, 10, 46})
+
+	fmt.Println(n)
+
+	for i := 0; i < 100; i++ {
+		n.ForwardStep()
+		n.BackwardStep()
+		fmt.Println("Cost: ", n.Cost())
+	}
+	for i, v := range n.yHat {
+		fmt.Println("yHat[", i, "]: ", v)
+	}
+	fmt.Println(n)
+
+}
